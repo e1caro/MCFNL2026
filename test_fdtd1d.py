cce34247a380835c7ebc9ca38107b1c1c44303eb
Alisama20 <alisalemstd@gmail.com>
2026-03-17 16:58:35 +0100

    Add periodic boundary conditions to FDTD1D
    - Update e[0] with wrap-around h nodes (h[0] and h[-1])
    - Enforce e[-1] = e[0] as same physical point
    - Add test_fdtd_periodic_boundary_conditions test

--- test_fdtd1d.py
+++ test_fdtd1d.py
@@ -63,17 +63,16 @@ def test_fdtd_periodic_boundary_conditions():
     x = np.linspace(xMin, xMax, 201)
     boundaries = ('periodic', 'periodic')
     
-    x0 = -0.5  
+    x0 = 0.0
     sigma = 0.05
     initial_e = gaussian(x, x0, sigma)
-    initial_h = np.zeros_like(initial_e)
+    initial_h = np.zeros_like(initial_e[:-1]) 
 
     fdtd = FDTD1D(x, boundaries)
     fdtd.load_initial_field(initial_e)
 
     L = xMax - xMin
-    t_final = L / C 
-
+    t_final = L / C
     fdtd.run_until(t_final)
 
     e_solved = fdtd.get_e()
@@ -82,8 +81,10 @@ def test_fdtd_periodic_boundary_conditions():
     e_expected = initial_e
     h_expected = initial_h
 
-    assert np.corrcoef(e_solved, e_expected)[0,1] > 0.99
+    assert np.corrcoef(e_solved, e_expected)[0, 1] > 0.99
     assert np.allclose(h_solved, h_expected, atol=1e-2)
+
+
 
 
 if __name__ == "__main__":
